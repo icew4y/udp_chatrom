8b207f4662e552b6c0af436de41a6efa00bf8627
DESKTOP-8A59JRS\weilai <weilaizhibentm@gmail.com>
2019-07-01 18:23:40 +0800

    wq

---
+++ client/client.cpp
@@ -0,0 +1,93 @@
+﻿// client.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <iostream>
+#include <assert.h>
+#include <thread>
+#include <chrono>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+#define SERVER 0
+#define ALL	1
+
+
+#define LOGIN 1000
+#define REGISTER 1001
+
+
+
+
+
+struct message
+{
+	uint8_t receiver;
+	uint16_t cmd;
+	char data[0];
+};
+
+struct Login
+{
+	char username[16];
+	char password[32];
+};
+
+
+char* performance_message(const char* p, size_t len, uint32_t* plen) {
+	assert(len > 0);
+	assert(p != nullptr);
+
+	char* newbuf = new char[len + sizeof(uint32_t)];
+	uint32_t newlen = len + sizeof(uint32_t);
+	memcpy(newbuf, &newlen, sizeof(uint32_t));
+	memcpy(newbuf + sizeof(uint32_t), p, len);
+	*plen = newlen;
+	return newbuf;
+}
+
+int main()
+{
+	WSADATA wsa;
+	int r = WSAStartup(MAKEWORD(2, 2), &wsa);
+
+
+	SOCKET fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+	sockaddr_in sin;
+	memset(&sin, 0, sizeof(sin));
+
+	inet_pton(AF_INET, "127.0.0.1", &sin.sin_addr);
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(8888);
+
+	int msglen = sizeof(message) + sizeof(Login);
+	message *msg = (message *)new char[msglen];
+	memset(msg, 0, msglen);
+	((message*)msg)->receiver = SERVER;
+	((message*)msg)->cmd = LOGIN;
+
+	Login* login = (Login*)((char *)msg + sizeof(message));
+	memcpy(login->username, "icew4y", strlen("icew4y"));
+	memcpy(login->password, "123456", strlen("123456"));
+	
+
+	uint32_t newlen = 0;
+	char* pmsg = performance_message((const char *)msg, msglen, &newlen);
+	delete msg;
+
+
+	while (true)
+	{
+		sendto(fd, pmsg, newlen, 0, (sockaddr*)& sin, sizeof(sin));
+		break;
+		this_thread::sleep_for(chrono::seconds(4));
+	}
+	
+	delete pmsg;
+}
+
---
+++ server/server.cpp
@@ -0,0 +1,138 @@
+﻿// server.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <string>
+
+#include "message_buf.h"
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+
+
+#define SERVER 0
+#define ALL	1
+
+
+#define LOGIN 1000
+#define REGISTER 1001
+
+
+
+
+
+struct message
+{
+	uint8_t receiver;
+	uint16_t cmd;
+	char data[0];
+};
+
+struct Login
+{
+	char username[16];
+	char password[32];
+};
+
+
+#define BUF_SIZE 1024
+
+void ServerReceiver(const SOCKET fd) {
+	sockaddr_in clientAddr;
+	int			addrLen = sizeof(clientAddr);
+
+	message_buf recvbuf;
+	int hsize = 0;
+
+	while (true) {
+
+		//UDP 直接从客户端接收数据
+		char buf[BUF_SIZE] = { 0 };
+		int irec = recvfrom(fd, buf, BUF_SIZE, 0, (sockaddr*)&clientAddr, &addrLen);
+		if (irec == SOCKET_ERROR) {
+			cout << "recvform failed wich code:" << WSAGetLastError() << endl;
+			break;
+		}
+
+		if (irec == 0)
+			continue;
+
+		recvbuf.add_msg(buf, irec);
+
+		if (hsize == 0) {
+			hsize = recvbuf.get_header(4);
+			if (hsize == 0)
+				continue;
+		}
+		int datalen = hsize - 4;
+		char* readbuf = new char[datalen];
+		int len = recvbuf.get_msg(readbuf, datalen);
+		if (len == 0) {
+			continue;
+		}
+
+
+		message* p_msg = (message*)readbuf;
+		switch (p_msg->receiver)
+		{
+		case SERVER: {
+			switch (p_msg->cmd)
+			{
+			case REGISTER:
+				break;
+			}
+			case LOGIN: {
+				cout << "LOGIN:" << endl;
+				Login* login = (Login*)p_msg->data;
+				cout << "username:" << login->username << endl;
+				cout << "password:" << login->password << endl;
+				break;
+			}
+			break;
+		}
+		case ALL: {
+			break;
+		}
+		default:
+			cout << "unknow recevier:" << p_msg->receiver << endl;
+			break;
+		}
+
+
+
+
+
+		hsize = 0;
+	}
+}
+
+int main()
+{
+	WSADATA wsa;
+	int r = WSAStartup(MAKEWORD(2,2), &wsa);
+
+
+	SOCKET fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+	sockaddr_in sin;
+	memset(&sin, 0, sizeof(sin));
+
+	inet_pton(AF_INET, "0.0.0.0", &sin.sin_addr);
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(8888);
+
+	r = ::bind(fd, (sockaddr*)&sin, sizeof(sin));
+	if (SOCKET_ERROR == r) {
+		cout << "bind failed with code:" << WSAGetLastError() << endl;
+		return 0;
+	}
+
+	ServerReceiver(fd);
+	closesocket(fd);
+	WSACleanup();
+}
+
